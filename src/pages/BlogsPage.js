172e0f09dfd237b4930da62a885f8c9aba44da97
Bach Nghia Nguyen <bachnghianguyen@gmail.com>
2021-06-11 09:10:49 +0700

    add blogs page

--- src/pages/BlogsPage.js
+++ src/pages/BlogsPage.js
@@ -1,9 +1,27 @@
 import React from "react";
+import allBlogs from "../components/allBlogs";
+import Title from "../components/Title";
 
 const BlogsPage = () => {
   return (
     <div>
-      <h1>Blogs Page</h1>
+      <div className="blog-title">
+        <Title title={"Recent Blogs"} span={"Recent Blogs"} />
+      </div>
+      <div className="blogs-page">
+        {allBlogs.map((blog) => {
+          return (
+            <div className="blog" key={blog.id}>
+              <div className="blog-content">
+                <img src={blog.image} alt="blog" />
+                <a href={blog.link} className="blog-link">
+                  {blog.title}
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
